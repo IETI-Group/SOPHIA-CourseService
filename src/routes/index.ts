d53c4a843afd6826fd649795e63d15c05351812f
Miguel Motta <93295363+MIGUEL-MOTTA-U@users.noreply.github.com>
2025-11-30 20:25:05 -0500

    feat: implement auth with jwt and cognito

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { type IRouter, Router } from 'express';
 import aispecs from './aispecs.js';
 import assignments from './assignments.js';
+import auth from './auth.js';
 import categories from './categories.js';
 import courses from './courses.js';
 import healthRoutes from './health.js';
@@ -14,6 +15,7 @@ const router: IRouter = Router();
 
 // Rutas de la aplicación
 router.use('/health', healthRoutes);
+router.use('/auth', auth);
 router.use(courses);
 router.use(lessons);
 router.use(quizzes);
